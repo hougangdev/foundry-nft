56bdbaa615bafc6f4f5efe0f4d77aa20da791df6
Wayne Chan <wayne@Waynes-MacBook-Pro.local>
2024-08-20 02:49:08 +0800

    commit test

--- script/DeployBasicNft.s.sol
+++ script/DeployBasicNft.s.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.18;
 import {Script, console} from "forge-std/Script.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 
+// Test
+
 contract DeployBasicNft is Script {
     function run() external returns (BasicNft) {
         vm.startBroadcast();
